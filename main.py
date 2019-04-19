a7dc598564cde8fb6f5d8fe09d99a14dbfd6364a
Alex <friedrichs.alexander+github@gmail.com>
2019-04-19 23:52:46 +0200

    Fehlerbehandlung und Logging

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import datetime
+import logging
 import os
 import requests
 import time
 
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from urllib.parse import urlencode, quote_plus
 from pathlib import Path
-from pprint import pprint
+#from pprint import pprint
+
+
+logger = logging.getLogger("postbank")
+logger.setLevel(logging.INFO)
+fh = logging.StreamHandler()
+fh.setLevel(logging.INFO)
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
 
 opts = Options()
 opts.headless = True
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " - Starte FireFox")
+logger.info("Starte FireFox")
 browser = Firefox(options=opts)
 
 #Setzen der Variablen
@@ -22,29 +34,38 @@ password = os.environ.get('PASSWORD')
 telegrambotkey = os.environ.get('TELEGRAMBOTKEY')
 chatid = os.environ.get('CHATID')
 
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " - Oeffne Startseite")
+logger.info("Öffne Startseite")
 
 # Startseite
 browser.get(postbankurl)
-time.sleep(15)
-browser.get_screenshot_as_file(picturepath + "/1.png")
+time.sleep(30)
+try:
+    browser.get_screenshot_as_file(picturepath + "/1.png")
+except WebDriverException:
+    logger.warning("Bild 1 konnte nicht gespeichert werden.")
 browser.find_element_by_id('username').send_keys(username)
 browser.find_element_by_id('password').send_keys(password)
 
 # Seite nach Login
 browser.find_element_by_xpath('/html/body/div[1]/div/main/div[2]/div[1]/div/form/div[2]/div[3]/button').submit()
-time.sleep(15)
-browser.get_screenshot_as_file(picturepath + "/2.png")
+time.sleep(30)
+try:
+    browser.get_screenshot_as_file(picturepath + "/2.png")
+except WebDriverException:
+    logger.warning("Bild 2 konnte nicht gespeichert werden.")
 balance = browser.find_element_by_xpath('/html/body/div[1]/div/main/div[2]/div[1]/div/div[2]/div/div/table/tbody/tr[1]/td[3]/span/span/span').text
 
-print(balance)
+logger.info(balance)
 
 # Logout
 browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/header/div/div/div').click()
-time.sleep(2)
+time.sleep(10)
 browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/nav[2]/div[2]/div[2]/ul/li[6]/a').click()
-time.sleep(15)
-browser.get_screenshot_as_file(picturepath + "/3.png")
+time.sleep(30)
+try:
+    browser.get_screenshot_as_file(picturepath + "/3.png")
+except WebDriverException:
+    logger.warning("Bild 3 konnte nicht gespeichert werden.")
 
 browser.quit()
 
@@ -69,7 +90,8 @@ if balancezahl != oldbalance:
     payload = {'chat_id': chatid, 'text': text}
     result = urlencode(payload, quote_via=quote_plus)
     r = requests.get("https://api.telegram.org/" + telegrambotkey + "/sendMessage?" + result)
-    pprint(r.json())
+    #pprint(r.json())
+    logger.info(r.json())
     filestore = open(picturepath + "/balance.txt", "w")
     filestore.write(balancezahl)
     filestore.close()
